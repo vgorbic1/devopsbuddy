45abdee30adfb30e23cf36e52de55602ae6d8da7
Vlad Gorbich <vladgorbich@gmail.com>
2017-01-30 18:01:03 -0600

    create i18n functionality

--- src/test/java/com/gorbich/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/gorbich/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -2,15 +2,32 @@ package com.gorbich.devopsbuddy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.gorbich.devopsbuddy.web.i18n.I18nService;
+
+import junit.framework.Assert;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DevopsbuddyApplication.class)
+@WebAppConfiguration
 public class DevopsbuddyApplicationTests {
+	
+	@Autowired
+	private I18nService i18NService;
 
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
 	}
 
 }
