a48c6e0f78f36f670967646e1330989b5e659ff6
Vlad Gorbich <vladgorbich@gmail.com>
2017-03-10 12:56:35 -0600

    add jpa

---
+++ src/main/java/com/gorbich/devopsbuddy/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,72 @@
+package com.gorbich.devopsbuddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+public class Role implements Serializable {
+
+	/** The Serial Version UID for Serializable Classes **/
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+	private String name;
+
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<UserRole> userRoles = new HashSet<>();
+
+	public Role() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
