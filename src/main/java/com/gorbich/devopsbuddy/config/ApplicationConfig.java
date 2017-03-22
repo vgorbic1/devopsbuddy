789b7ba62baeaeda334fb2a4a2d0ebdd952593ec
Vlad Gorbich <vladgorbich@gmail.com>
2017-03-22 10:08:09 -0500

    add AWS S3 functionality

--- src/main/java/com/gorbich/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/gorbich/devopsbuddy/config/ApplicationConfig.java
@@ -1,10 +1,18 @@
 package com.gorbich.devopsbuddy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.gorbich.devopsbuddy.backend.persistence.repositories")
@@ -13,4 +21,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @PropertySource("file:///D:/STS/.devopsbuddy/application-common.properties")
 public class ApplicationConfig {
 
+    @Value("${aws.s3.profile}")
+    private String awsProfileName;
+
+    @Bean
+    public AmazonS3Client s3Client() {
+        AWSCredentials credentials = new ProfileCredentialsProvider(awsProfileName).getCredentials();
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        Region region = Region.getRegion(Regions.US_EAST_2);
+        s3Client.setRegion(region);
+        return s3Client;
+    }
 }
--- src/main/java/com/gorbich/devopsbuddy/web/controllers/SignupController.java
+++ src/main/java/com/gorbich/devopsbuddy/web/controllers/SignupController.java
@@ -20,12 +20,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gorbich.devopsbuddy.backend.persistence.domain.backend.Plan;
 import com.gorbich.devopsbuddy.backend.persistence.domain.backend.Role;
 import com.gorbich.devopsbuddy.backend.persistence.domain.backend.User;
 import com.gorbich.devopsbuddy.backend.persistence.domain.backend.UserRole;
 import com.gorbich.devopsbuddy.backend.service.PlanService;
+import com.gorbich.devopsbuddy.backend.service.S3Service;
 import com.gorbich.devopsbuddy.backend.service.UserService;
 import com.gorbich.devopsbuddy.enums.PlansEnum;
 import com.gorbich.devopsbuddy.enums.RolesEnum;
@@ -35,137 +37,152 @@ import com.gorbich.devopsbuddy.web.domain.frontend.ProAccountPayload;
 
 @Controller
 public class SignupController {
+
+	@Autowired
+	private PlanService planService;
+
+	@Autowired
+	private UserService userService;
 	
     @Autowired
-    private PlanService planService;
+    private S3Service s3Service;
 
-    @Autowired
-    private UserService userService;
-	
-    /** The application logger */
-    private static final Logger LOG = LoggerFactory.getLogger(SignupController.class);
-    
-    public static final String SIGNUP_URL_MAPPING = "/signup";
+	/** The application logger */
+	private static final Logger LOG = LoggerFactory.getLogger(SignupController.class);
 
-    public static final String PAYLOAD_MODEL_KEY_NAME = "payload";
-    
-    public static final String SUBSCRIPTION_VIEW_NAME = "registration/signup";
-    
-    public static final String DUPLICATED_USERNAME_KEY = "duplicatedUsername";
+	public static final String SIGNUP_URL_MAPPING = "/signup";
 
-    public static final String DUPLICATED_EMAIL_KEY = "duplicatedEmail";
-    
-    public static final String SIGNED_UP_MESSAGE_KEY = "signedUp";
+	public static final String PAYLOAD_MODEL_KEY_NAME = "payload";
 
-    public static final String ERROR_MESSAGE_KEY = "message";
-    
-    @RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.GET)
-    public String signupGet(@RequestParam("planId") int planId, ModelMap model) {
+	public static final String SUBSCRIPTION_VIEW_NAME = "registration/signup";
 
-        if (planId != PlansEnum.BASIC.getId() && planId != PlansEnum.PRO.getId()) {
-            throw new IllegalArgumentException("Plan id is not valid");
-        }
-        model.addAttribute(PAYLOAD_MODEL_KEY_NAME, new ProAccountPayload());
+	public static final String DUPLICATED_USERNAME_KEY = "duplicatedUsername";
 
-        return SUBSCRIPTION_VIEW_NAME;
-    }
-    
-    @RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.POST)
-    public String signUpPost(@RequestParam(name = "planId", required = true) int planId,
-                             @ModelAttribute(PAYLOAD_MODEL_KEY_NAME) @Valid ProAccountPayload payload,
-                             ModelMap model) throws IOException {
+	public static final String DUPLICATED_EMAIL_KEY = "duplicatedEmail";
 
-        if (planId != PlansEnum.BASIC.getId() && planId != PlansEnum.PRO.getId()) {
-            model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
-            model.addAttribute(ERROR_MESSAGE_KEY, "Plan id does not exist");
-            return SUBSCRIPTION_VIEW_NAME;
-        }
+	public static final String SIGNED_UP_MESSAGE_KEY = "signedUp";
 
-        this.checkForDuplicates(payload, model);
+	public static final String ERROR_MESSAGE_KEY = "message";
 
-        boolean duplicates = false;
+	@RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.GET)
+	public String signupGet(@RequestParam("planId") int planId, ModelMap model) {
 
-        List<String> errorMessages = new ArrayList<>();
+		if (planId != PlansEnum.BASIC.getId() && planId != PlansEnum.PRO.getId()) {
+			throw new IllegalArgumentException("Plan id is not valid");
+		}
+		model.addAttribute(PAYLOAD_MODEL_KEY_NAME, new ProAccountPayload());
 
-        if (model.containsKey(DUPLICATED_USERNAME_KEY)) {
-            LOG.warn("The username already exists. Displaying error to the user");
-            model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
-            errorMessages.add("Username already exist");
-            duplicates = true;
-        }
+		return SUBSCRIPTION_VIEW_NAME;
+	}
 
-        if (model.containsKey(DUPLICATED_EMAIL_KEY)) {
-            LOG.warn("The email already exists. Displaying error to the user");
-            model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
-            errorMessages.add("Email already exist");
-            duplicates = true;
-        }
+	@RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.POST)
+	public String signUpPost(@RequestParam(name = "planId", required = true) int planId,
+			@RequestParam(name = "file", required = false) MultipartFile file,
+			@ModelAttribute(PAYLOAD_MODEL_KEY_NAME) @Valid ProAccountPayload payload, ModelMap model)
+			throws IOException {
 
-        if (duplicates) {
-            model.addAttribute(ERROR_MESSAGE_KEY, errorMessages);
-            return SUBSCRIPTION_VIEW_NAME;
-        }
+		if (planId != PlansEnum.BASIC.getId() && planId != PlansEnum.PRO.getId()) {
+			model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
+			model.addAttribute(ERROR_MESSAGE_KEY, "Plan id does not exist");
+			return SUBSCRIPTION_VIEW_NAME;
+		}
 
+		this.checkForDuplicates(payload, model);
 
-        // There are certain info that the user doesn't set, such as profile image URL, Stripe customer id,
-        // plans and roles
-        LOG.debug("Transforming user payload into User domain object");
-        User user = UserUtils.fromWebUserToDomainUser(payload);
+		boolean duplicates = false;
 
-        // Sets the Plan and the Roles (depending on the chosen plan)
-        LOG.debug("Retrieving plan from the database");
-        Plan selectedPlan = planService.findPlanById(planId);
-        if (null == selectedPlan) {
-            LOG.error("The plan id {} could not be found. Throwing exception.", planId);
-            model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
-            model.addAttribute(ERROR_MESSAGE_KEY, "Plan id not found");
-            return SUBSCRIPTION_VIEW_NAME;
-        }
-        user.setPlan(selectedPlan);
+		List<String> errorMessages = new ArrayList<>();
 
-        User registeredUser = null;
+		if (model.containsKey(DUPLICATED_USERNAME_KEY)) {
+			LOG.warn("The username already exists. Displaying error to the user");
+			model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
+			errorMessages.add("Username already exist");
+			duplicates = true;
+		}
 
-        // By default users get the BASIC ROLE
-        Set<UserRole> roles = new HashSet<>();
-        if (planId == PlansEnum.BASIC.getId()) {
-            roles.add(new UserRole(user, new Role(RolesEnum.BASIC)));
-            registeredUser = userService.createUser(user, PlansEnum.BASIC, roles);
-        } else {
-            roles.add(new UserRole(user, new Role(RolesEnum.PRO)));
-            registeredUser = userService.createUser(user, PlansEnum.PRO, roles);
-            LOG.debug(payload.toString());
+		if (model.containsKey(DUPLICATED_EMAIL_KEY)) {
+			LOG.warn("The email already exists. Displaying error to the user");
+			model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
+			errorMessages.add("Email already exist");
+			duplicates = true;
+		}
+
+		if (duplicates) {
+			model.addAttribute(ERROR_MESSAGE_KEY, errorMessages);
+			return SUBSCRIPTION_VIEW_NAME;
+		}
+
+		// There are certain info that the user doesn't set, such as profile
+		// image URL, Stripe customer id,
+		// plans and roles
+		LOG.debug("Transforming user payload into User domain object");
+		User user = UserUtils.fromWebUserToDomainUser(payload);
+
+		// Stores the profile image on Amazon S3 and stores the URL in the user's record
+        if (file != null && !file.isEmpty()) {
+
+            String profileImageUrl = s3Service.storeProfileImage(file, payload.getUsername());
+            if (profileImageUrl != null) {
+                user.setProfileImageUrl(profileImageUrl);
+            } else {
+                LOG.warn("There was a problem uploading the profile image to S3. The user's profile will" +
+                        " be created without the image");
+            }
+
         }
 
+		// Sets the Plan and the Roles (depending on the chosen plan)
+		LOG.debug("Retrieving plan from the database");
+		Plan selectedPlan = planService.findPlanById(planId);
+		if (null == selectedPlan) {
+			LOG.error("The plan id {} could not be found. Throwing exception.", planId);
+			model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
+			model.addAttribute(ERROR_MESSAGE_KEY, "Plan id not found");
+			return SUBSCRIPTION_VIEW_NAME;
+		}
+		user.setPlan(selectedPlan);
 
-        // Auto logins the registered user
-        Authentication auth = new UsernamePasswordAuthenticationToken(
-                registeredUser, null, registeredUser.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(auth);
+		User registeredUser = null;
 
-        LOG.info("User created successfully");
+		// By default users get the BASIC ROLE
+		Set<UserRole> roles = new HashSet<>();
+		if (planId == PlansEnum.BASIC.getId()) {
+			roles.add(new UserRole(user, new Role(RolesEnum.BASIC)));
+			registeredUser = userService.createUser(user, PlansEnum.BASIC, roles);
+		} else {
+			roles.add(new UserRole(user, new Role(RolesEnum.PRO)));
+			registeredUser = userService.createUser(user, PlansEnum.PRO, roles);
+			LOG.debug(payload.toString());
+		}
 
-        model.addAttribute(SIGNED_UP_MESSAGE_KEY, "true");
+		// Auto logins the registered user
+		Authentication auth = new UsernamePasswordAuthenticationToken(registeredUser, null,
+				registeredUser.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(auth);
 
-        return SUBSCRIPTION_VIEW_NAME;
-    }
+		LOG.info("User created successfully");
 
+		model.addAttribute(SIGNED_UP_MESSAGE_KEY, "true");
 
-    //--------------> Private methods
+		return SUBSCRIPTION_VIEW_NAME;
+	}
 
-    /**
-     * Checks if the username/email are duplicates and sets error flags in the model.
-     * Side effect: the method might set attributes on Model
-     **/
-    private void checkForDuplicates(BasicAccountPayload payload, ModelMap model) {
+	// --------------> Private methods
 
-        // Username
-        if (userService.findByUserName(payload.getUsername()) != null) {
-            model.addAttribute(DUPLICATED_USERNAME_KEY, true);
-        }
-        if (userService.findByEmail(payload.getEmail()) != null) {
-            model.addAttribute(DUPLICATED_EMAIL_KEY, true);
-        }
+	/**
+	 * Checks if the username/email are duplicates and sets error flags in the
+	 * model. Side effect: the method might set attributes on Model
+	 **/
+	private void checkForDuplicates(BasicAccountPayload payload, ModelMap model) {
 
-    }
-    
+		// Username
+		if (userService.findByUserName(payload.getUsername()) != null) {
+			model.addAttribute(DUPLICATED_USERNAME_KEY, true);
+		}
+		if (userService.findByEmail(payload.getEmail()) != null) {
+			model.addAttribute(DUPLICATED_EMAIL_KEY, true);
+		}
+
+	}
+
 }
